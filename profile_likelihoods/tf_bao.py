f98c6937597d0a1e8e8bde8db9ccd8bf0108dd96
Luca Janken <137942454+LucaJanken@users.noreply.github.com>
2023-11-20 12:20:48 +0100

    Add files via upload

---
+++ profile_likelihoods/tf_bao.py
@@ -0,0 +1,73 @@
+import os
+import numpy as np
+import tensorflow as tf
+import pickle as pkl
+
+class TF_bao():
+
+    # Initialization routine
+    def __init__(self, model_name):
+
+        # Load CONNECT model
+        self.model = tf.keras.models.load_model(model_name, compile=False)
+        self.output_info = eval(self.model.get_raw_info().numpy().decode('utf-8'))
+
+        # Set path to datafile (Hardcoded paths would be avoided for general implementation)
+        self.data_directory = '/home/lucajn/connect/resources/montepython_public/data/'
+        self.file = 'bao_2012.txt'
+        self.rs_rescale = 153.017 / 149.0808 # From tf_bao.data file
+
+        # Define emulated data
+        self.ang_idx = self.output_info['interval']['bg']['ang.diam.dist.'][:2]
+        self.hubble_idx = self.output_info['interval']['bg']['H [1/Mpc]'][:2]
+        self.rs_drag_idx = self.output_info['interval']['extra']['rs_drag'][0]
+
+        # Define array for values of z and data points
+        self.z = np.array([], 'float64')
+        self.data = np.array([], 'float64')
+        self.error = np.array([], 'float64')
+        self.type = np.array([], 'int')
+
+        # Read redshifts and data points
+        for line in open(os.path.join(self.data_directory, self.file), 'r'):
+            if (line.find('#') == -1):
+                self.z = np.append(self.z, float(line.split()[0]))
+                self.data = np.append(self.data, float(line.split()[1]))
+                self.error = np.append(self.error, float(line.split()[2]))
+                self.type = np.append(self.type, int(line.split()[3]))
+
+        # Number of data points
+        self.num_points = np.shape(self.z)[0]
+
+    @tf.function
+    def loglkl(self, x):
+        
+        # Define output
+        output = self.model(x[:,:-1])
+
+        # Calculations
+        hubble = output[:, self.hubble_idx[0]:self.hubble_idx[1]]
+        da = output[:, self.ang_idx[0]:self.ang_idx[1]]
+        dr = tf.divide(self.z, hubble)
+        dv = tf.pow(da * da * (1 + self.z) * (1 + self.z) * dr, 1. / 3.)
+        rs_drag = output[:,self.rs_drag_idx:self.rs_drag_idx+1]
+
+        # Initialize a tensor for the 'theo' values
+        theo = tf.zeros_like(self.z, dtype=tf.float32)
+        
+        # Handle the different types using boolean masks
+        mask_type3 = tf.math.equal(self.type, 3)
+        rs = rs_drag * self.rs_rescale
+        theo = tf.where(mask_type3, dv / rs, theo)
+        
+        mask_type4 = tf.math.equal(self.type, 4)
+        theo = tf.where(mask_type4, dv, theo)
+
+        # Compute chi2
+        chi2_values = ((theo - self.data) / self.error) ** 2
+        chi2 = tf.reduce_sum(chi2_values, 1, keepdims=True)
+
+        # Compute ln(L)
+        lkl = -0.5 * chi2
+        
+        return lkl
---
+++ profile_likelihoods/tf_bao_dr12.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+import warnings
+import scipy.constants as conts
+import tensorflow as tf
+import pickle as pkl
+
+class TF_bao_boss_dr12():
+
+    # Initialization routine
+    def __init__(self, model_name):
+
+        # Load CONNECT model
+        self.model = tf.keras.models.load_model(model_name, compile=False)
+        self.output_info = eval(self.model.get_raw_info().numpy().decode('utf-8'))
+        self.rs_rescale, self.rd_fid_in_Mpc = 1. , 147.78
+
+        # Define emulated data
+        self.ang_idx = self.output_info['interval']['bg']['ang.diam.dist.'][:2]
+        self.hubble_idx = self.output_info['interval']['bg']['H [1/Mpc]'][:2]
+        self.rs_drag_idx = self.output_info['interval']['extra']['rs_drag'][0]
+
+        # Set path to datafile (Hardcoded paths would be avoided for general implementation)
+        self.data_directory = 'data/COMBINEDDR12_BAO_consensus_dM_Hz/'
+        self.data_file = 'BAO_consensus_results_dM_Hz.txt'
+        self.cov_file = 'BAO_consensus_covtot_dM_Hz.txt'
+
+        data = np.loadtxt(os.path.join(self.data_directory, self.data_file), skiprows=1, usecols=(0,2))
+        self.z = np.array(list(set(data[:,0])))
+        self.DM_rdfid_by_rd_in_Mpc, self.H_rd_by_rdfid_in_km_per_s_per_Mpc = np.reshape(data[:,1], [3,2]).T
+
+        # Read covariance matrix
+        self.cov_data = np.loadtxt(os.path.join(self.data_directory, self.cov_file))
+        self.inv_cov_data = tf.cast(tf.linalg.inv(self.cov_data), tf.float32)
+        # end of initialization
+
+    # compute likelihood
+    @tf.function
+    def loglkl(self, x):
+        # Define output
+        output = self.model(x[:,:])
+        
+        # Compute comoving angular diameter distance D_M = (1 + z) * D_A and Hubble values for each z
+        hubble = output[:, self.hubble_idx[0]:self.hubble_idx[1]]
+        da = output[:, self.ang_idx[0]:self.ang_idx[1]]
+        DM_at_z_values = da * (1. + self.z)
+        H_at_z_values = hubble * conts.c / 1000.0
+        rs_drag = output[:,self.rs_drag_idx:self.rs_drag_idx+1]
+
+        # Compute sound horizon at baryon drag rs_d
+        rd = rs_drag * self.rs_rescale
+
+        # Compute theoretical predictions
+        theo_DM_rdfid_by_rd_in_Mpc = DM_at_z_values / rd * self.rd_fid_in_Mpc
+        theo_H_rd_by_rdfid = H_at_z_values * rd / self.rd_fid_in_Mpc
+
+        # Compute differences with observations
+        DM_diff = tf.transpose(theo_DM_rdfid_by_rd_in_Mpc - self.DM_rdfid_by_rd_in_Mpc)
+        H_diff = tf.transpose(theo_H_rd_by_rdfid - self.H_rd_by_rdfid_in_km_per_s_per_Mpc)
+
+        # Stack DM_diff and H_diff into a single array
+        data_array = tf.transpose(tf.dynamic_stitch([[0,2,4],[1,3,5]], [DM_diff, H_diff]))
+
+        # Compute chi squared
+        chi2 = tf.tensordot(tf.tensordot(data_array, self.inv_cov_data,1), tf.transpose(data_array), 1)
+        
+        # Return ln(L)
+        loglkl = -0.5 * chi2
+        
+        return loglkl
